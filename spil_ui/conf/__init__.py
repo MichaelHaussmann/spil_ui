9d0f2fba25e8455acf5b37383cb5bf7b2c8ea3fa
michael <git@xeo.info>
2023-01-22 00:32:38 +0100

    Applied Black

--- spil_ui/conf/__init__.py
+++ spil_ui/conf/__init__.py
@@ -13,38 +13,58 @@ If not, see <https://www.gnu.org/licenses/>.
 """
 
 sid_usage_history_len = 20
-application_name = 'Spil'
-browser_title = 'Browser @ Spil'
+application_name = "Spil"
+browser_title = "Browser @ Spil"
 
 # Configuration for the Browser
 
-searchers = ['*', ',', '>', '<', '**']
-
-table_bloc_columns = ['Sid']  # , 'Comment', 'Size', 'Time']
+table_bloc_columns = ["Sid"]  # , 'Comment', 'Size', 'Time']
 table_bloc_attributes = []  # 'comment', 'size', 'time']
-search_reset_keys = ['project', 'type']  # , 'assettype', 'sequence']  # these fields trigger a reset of the search sid - else we are "sticky" and only change the given key.
 
-# key that separates entity lists (left side) and the version table (right side), depending of the search's basetype
-basetype_to_cut = {'default': 'tasktype'}
+# these fields trigger a reset of the search sid - else we are "sticky" and only change the given key.
+search_reset_keys = [
+    "project",
+    "type",
+]  # , 'assettype', 'sequence']
 
-# usually, the 'ext' is considered the "leaf" (last) key of a Sid. Depending on the basetype, we can override this.
+# key that separates entity lists (left side) and the version table (right side),
+# depending on the search's basetype
+basetype_to_cut = {"default": "tasktype"}
+
+# usually, the 'ext' is considered the "leaf" (last) key of a Sid.
+# Depending on the basetype, we can override this.
 basetype_to_leafkey = {}  # {'render': 'layer'}
 
 # for some search types, the version search gets "clipped", meaning "**" is switched to "*"
+# Usefull to handle file sequences by using at the parent folder
 basetype_clipped_versions = []  # ['render']
 
-extension_filters = ['maya', 'hou', 'blend', 'movie', 'cache', 'img', 'nk']  # 'nk', 'spp'
+# creates checkboxes to filter file extensions
+extension_filters = [
+    "maya",
+    "hou",
+    "blend",
+    "movie",
+    "cache",
+    "img",
+    "nk",
+]  # 'nk', 'spp'
 
 
-#  "leaf" means the last key of a Sid. Typically the extension "ext". Can be overridden depending on type.
+#  "leaf" means the last key of a Sid. Typically the extension "ext".
+#  Can be overridden depending on type.
 def is_leaf(sid):
-    return sid.is_leaf() or sid.keytype == basetype_to_leafkey.get(sid.basetype, 'ext')
+    return sid.is_leaf() or sid.keytype == basetype_to_leafkey.get(sid.basetype, "ext")
 
 
+# function to return an ActionHandler object
 def get_action_handler():
     try:
+        # uses the spil action framework
         from spil_action.libs.ui.action_handler import EngineActionHandler
+
         return EngineActionHandler()
     except Exception as ex:
         from spil_ui.browser.ui.action_handler import ExampleActionHandler
+
         return ExampleActionHandler()
--- spil_ui/util/dialogs.py
+++ spil_ui/util/dialogs.py
@@ -16,7 +16,14 @@ Created on 22 dec. 2011
 
 """
 # Uses Qt.py
-from Qt.QtWidgets import QMessageBox, QApplication, QWidget, QFileDialog, QInputDialog, QLineEdit
+from Qt.QtWidgets import (
+    QMessageBox,
+    QApplication,
+    QWidget,
+    QFileDialog,
+    QInputDialog,
+    QLineEdit,
+)
 from Qt.QtWidgets import QDialog
 from Qt.QtWidgets import QMainWindow
 from Qt.QtCore import QCoreApplication
@@ -24,12 +31,11 @@ from Qt.QtCore import QCoreApplication
 from spil_ui.conf import application_name
 
 
-class conf():
+class conf:
     application_name = application_name
 
 
 class ChoiceBox(QDialog):
-
     def __init__(self, choiceA, choiceB, title, windowTitle, parent=None):
         super(ChoiceBox, self).__init__(parent)
         self.msgBox = QMessageBox()
@@ -42,7 +48,6 @@ class ChoiceBox(QDialog):
 
 
 class MultiChoiceBox(QDialog):
-
     def __init__(self, choices, title, windowTitle, parent=None):
         super(MultiChoiceBox, self).__init__(parent)
         self.msgBox = QMessageBox()
@@ -72,19 +77,25 @@ class Dialogs(QWidget):
         super(Dialogs, self).__init__()
 
     def striplog(self, message):
-        return str(message).split('] ', 1)[-1]
+        return str(message).split("] ", 1)[-1]
 
     def inform(self, message):
-        """ Opens an information window with a confirm button """
-        QMessageBox.information(self, conf.application_name,
-                                QApplication.translate("Dialogs", self.striplog(message), None),
-                                QMessageBox.Ok)
+        """Opens an information window with a confirm button"""
+        QMessageBox.information(
+            self,
+            conf.application_name,
+            QApplication.translate("Dialogs", self.striplog(message), None),
+            QMessageBox.Ok,
+        )
 
     def error(self, message):
-        """ Opens an critical (error) window with a confirm button """
-        QMessageBox.critical(self, conf.application_name,
-                             QApplication.translate("Dialogs", self.striplog(message), None),
-                             QMessageBox.Ok)
+        """Opens an critical (error) window with a confirm button"""
+        QMessageBox.critical(
+            self,
+            conf.application_name,
+            QApplication.translate("Dialogs", self.striplog(message), None),
+            QMessageBox.Ok,
+        )
 
     def confirm(self, message, withCancel=False):
         """
@@ -92,9 +103,12 @@ class Dialogs(QWidget):
         If withCancel is True, a Cancel button is shown, which triggers a None return.
         """
         if withCancel:
-            ret = QMessageBox.warning(self, conf.application_name,
-                                      QApplication.translate("Dialogs", self.striplog(message), None),
-                                      QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel)
+            ret = QMessageBox.warning(
+                self,
+                conf.application_name,
+                QApplication.translate("Dialogs", self.striplog(message), None),
+                QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel,
+            )
             if ret == QMessageBox.Yes:
                 return True
             if ret == QMessageBox.No:
@@ -102,42 +116,60 @@ class Dialogs(QWidget):
             if ret == QMessageBox.Cancel:
                 return None
         else:
-            ret = QMessageBox.question(self, conf.application_name,
-                                   QApplication.translate("Dialogs", self.striplog(message), None),
-                                   QMessageBox.Yes | QMessageBox.No)
+            ret = QMessageBox.question(
+                self,
+                conf.application_name,
+                QApplication.translate("Dialogs", self.striplog(message), None),
+                QMessageBox.Yes | QMessageBox.No,
+            )
         return ret == QMessageBox.Yes
 
     def warn(self, message, withCancel=False):
-        """ Opens an Ok/Cancel warning window and returns a Boolean """
+        """Opens an Ok/Cancel warning window and returns a Boolean"""
         if withCancel:
-            ret = QMessageBox.warning(self, conf.application_name,
-                                      QApplication.translate("Dialogs", self.striplog(message), None),
-                                      QMessageBox.Ok | QMessageBox.Cancel)
+            ret = QMessageBox.warning(
+                self,
+                conf.application_name,
+                QApplication.translate("Dialogs", self.striplog(message), None),
+                QMessageBox.Ok | QMessageBox.Cancel,
+            )
         else:
-            ret = QMessageBox.warning(self, conf.application_name,
-                                      QApplication.translate("Dialogs", self.striplog(message), None),
-                                      QMessageBox.Ok)
+            ret = QMessageBox.warning(
+                self,
+                conf.application_name,
+                QApplication.translate("Dialogs", self.striplog(message), None),
+                QMessageBox.Ok,
+            )
         return ret == QMessageBox.Ok
 
     def killer(self, message):
-        """ Opens an critical (error) window with a confirm button, and kills the application """
-        QMessageBox.critical(self, conf.application_name,
-                             QApplication.translate("Dialogs", self.striplog(message), None),
-                             QMessageBox.Ok)
-        sys.exit('[Dialogs.killer] System was stopped. "{}"'.format(message))
+        """Opens an critical (error) window with a confirm button, and kills the application"""
+        QMessageBox.critical(
+            self,
+            conf.application_name,
+            QApplication.translate("Dialogs", self.striplog(message), None),
+            QMessageBox.Ok,
+        )
+        sys.exit(f'[Dialogs.killer] System was stopped. "{message}"')
 
-    def getOpenFileName(self, message, filter="Images (*.png *.xpm *.jpg);;Text files (*.txt)"):
+    def getOpenFileName(
+        self, message, filter="Images (*.png *.xpm *.jpg);;Text files (*.txt)"
+    ):
         # "Images (*.png *.xpm *.jpg);;Text files (*.txt);;XML files (*.xml)"
-        result = QFileDialog.getOpenFileName(self, self.tr(message), "", self.tr(filter))
+        result = QFileDialog.getOpenFileName(
+            self, self.tr(message), "", self.tr(filter)
+        )
         return result[0]
 
-    def getText(self, label='Please enter', default=''):
+    def getText(self, label="Please enter", default=""):
 
         # value, boul = QInputDialog.getText(self,  conf.application_name, QApplication.translate("Dialogs", str(label), str(default)))
-        value, ok = QInputDialog.getText(self, conf.application_name, label, QLineEdit.Normal, default)
+        value, ok = QInputDialog.getText(
+            self, conf.application_name, label, QLineEdit.Normal, default
+        )
         return value, ok
 
-    def getTextField(self, label='Please enter', default='', size=(300, 300)):
+    def getTextField(self, label="Please enter", default="", size=(300, 300)):
         dlg = QInputDialog(self)
         dlg.setWindowTitle(conf.application_name)
         dlg.setInputMode(QInputDialog.TextInput)
@@ -146,7 +178,7 @@ class Dialogs(QWidget):
         dlg.setLabelText(label)
         dlg.resize(*size)
         ok = dlg.exec_()
-        return dlg.textValue() if ok else ''
+        return dlg.textValue() if ok else ""
 
     def choice(self, message, options, default=None):
         """
@@ -156,14 +188,16 @@ class Dialogs(QWidget):
         if default and default in options:
             index = options.index(default)
 
-        result, ok = QInputDialog.getItem(self, conf.application_name, message, options, index)
+        result, ok = QInputDialog.getItem(
+            self, conf.application_name, message, options, index
+        )
         return str(result) if ok else False
 
     @staticmethod
-    def buttonChoice(choiceA, choiceB, title='', windowTitle=conf.application_name):
-        '''
+    def buttonChoice(choiceA, choiceB, title="", windowTitle=conf.application_name):
+        """
         Returns True (choice A), False (choice B) or None (Cancel)
-        '''
+        """
         fxMsgBox = ChoiceBox(choiceA, choiceB, title=title, windowTitle=windowTitle)
         fxMsgBox.msgBox.exec_()
 
@@ -177,19 +211,19 @@ class Dialogs(QWidget):
             return None
 
     @staticmethod
-    def multiButtonChoice(choices, title='', windowTitle=conf.application_name):
-        '''
+    def multiButtonChoice(choices, title="", windowTitle=conf.application_name):
+        """
         Returns the index of the choice, starting at 1
         None (Cancel)
-        '''
+        """
         fxMsgBox = MultiChoiceBox(choices, title=title, windowTitle=windowTitle)
         fxMsgBox.msgBox.exec_()
 
         for choice in choices:
             if fxMsgBox.msgBox.clickedButton() == getattr(fxMsgBox, choice):
-                return (choices.index(choice) + 1)
+                return choices.index(choice) + 1
 
-    def getFromUi(self, ui, getter='sid'):
+    def getFromUi(self, ui, getter="sid"):
         """
         Simple little wrapper for UIs to return a value.
 
@@ -203,18 +237,22 @@ class Dialogs(QWidget):
             result = Dialogs().getFromUi(ui=cui.CreateAssetUI(sid), getter='result_sid')
         """
         if isinstance(ui, QMainWindow):
-            raise Exception('[Dialogs.getFromUi] The given UI must not be a MainWindow.\nGiven {}'.format(ui.__class__))
+            raise Exception(
+                f"[Dialogs.getFromUi] The given UI must not be a MainWindow.\nGiven {ui.__class__}"
+            )
         try:
             ui.show()
             ui.exec_()
             return getattr(ui, getter)
         except Exception as ex:
-            raise Exception('[Dialogs.getFromUi] Unable to get value from ui : "{}"'.format(ex))
+            raise Exception(
+                '[Dialogs.getFromUi] Unable to get value from ui : "{}"'.format(ex)
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    ''' tests only '''
+    """tests only"""
     import sys
 
     app = QApplication(sys.argv)
@@ -228,16 +266,38 @@ if __name__ == '__main__':
     print(result)
     """
 
-    print(Dialogs().getTextField('Please enter a very long text here: ', '...', size=(200, 400)))
-    print(Dialogs().getText('Default choice', 'Not a bad choice either'))
+    print(
+        Dialogs().getTextField(
+            "Please enter a very long text here: ", "...", size=(200, 400)
+        )
+    )
+    print(Dialogs().getText("Default choice", "Not a bad choice either"))
 
     # print Dialogs.buttonChoice('Use last saved version', 'Use current scene', title='Current scene was modified... what would you like to do?', windowTitle='Tomato')
-    print(Dialogs.multiButtonChoice(['Default choice', 'Not a bad choice either', 'utlimatively best choice'], title='Up to you... \nWhat would you like to do?', windowTitle='Tomato'))
+    print(
+        Dialogs.multiButtonChoice(
+            ["Default choice", "Not a bad choice either", "utlimatively best choice"],
+            title="Up to you... \nWhat would you like to do?",
+            windowTitle="Tomato",
+        )
+    )
 
     print(Dialogs().warn("Are you sure you want to start this?"))
-    print(Dialogs().choice("choose now between all this crap", ['absolut', 'omega', 'jack'], 'toto'))
-    print(Dialogs().choice("choose now between all this crap", ['absolut', 'omega', 'jack'], 'omeg'))
-    print(Dialogs().choice("choose now between all this crap", ['absolut', 'omega', 'jack'], 'omega'))
+    print(
+        Dialogs().choice(
+            "choose now between all this crap", ["absolut", "omega", "jack"], "toto"
+        )
+    )
+    print(
+        Dialogs().choice(
+            "choose now between all this crap", ["absolut", "omega", "jack"], "omeg"
+        )
+    )
+    print(
+        Dialogs().choice(
+            "choose now between all this crap", ["absolut", "omega", "jack"], "omega"
+        )
+    )
 
     print(Dialogs().inform("Ok, good answer"))
     print(Dialogs().confirm("Do you like user interfaces?"))
@@ -245,6 +305,5 @@ if __name__ == '__main__':
     print(Dialogs().error("No, no, no... That was a mistake!"))
     print(Dialogs().getOpenFileName("Ah I forgot... please upload"))
     print(Dialogs().killer("This is the end my friend .... "))
-    print('Done')
+    print("Done")
 #     sys.exit(app.exec_())
-
