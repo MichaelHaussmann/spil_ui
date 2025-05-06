698c8d6d592780009fc843f4b3c12b554e8867c3
Michael Haussmann <git@xeo.info>
2025-05-06 12:47:44 +0200

    add rez alias to launch browser and bar, patch version bump to 0.1.2

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = 'spil_ui'
 
-version = '0.1.0'
+version = '0.1.2'
 
 requires = [
     "spil",
@@ -18,6 +18,9 @@ authors = ['Michael Haussmann']
 
 def commands():
     env.PYTHONPATH.append('{root}')
+
+    alias("browser", 'python -c "from spil_ui import app;app()"')
+    alias("bar", 'python -c "from spil_ui import bar;bar()"')
 
 
 is_pure_python = True
